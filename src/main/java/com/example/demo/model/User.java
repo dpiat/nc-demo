4f975e4a63a6eef9cb4aa0356e9e5d3fe7d1e44e
dpiat <dpiataikin@gmail.com>
2020-11-23 19:07:59 +0300

    init commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 15)
+    private String username;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+}
